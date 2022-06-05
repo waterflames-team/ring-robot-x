b594cb4d356727a49e6bb238ee312922954ca2b4
折腾调 <1410007625@qq.com>
2022-06-05 15:09:05 +0800

    fix: 好习惯

--- model/asr.py
+++ model/asr.py
@@ -1,5 +1,5 @@
 import model.hook
-
+import model.player
 
 def audioRecorderCallback(fname):
     """
@@ -8,7 +8,9 @@ def audioRecorderCallback(fname):
     :return: 无
     """
     if model.config.fastGetConfig("func")["dontshout"]:
+        model.player.dele(fname)
         return
     model.hook.runhook_fast("RRCore.Model.Before.ASRRunning", fname)
     model.hook.runhook_fast("RRCore.Func.ASR", fname)
     model.hook.runhook_fast("RRCore.Model.After.ASRRunning", fname)
+    model.player.dele(fname)

41d063b804b30506546033c13f9fbff8c8d9196c
Epeiuss <2822603942@qq.com>
2022-07-02 01:32:40 +0800

    🎨 主文件格式化

--- ring.py
+++ ring.py
@@ -6,7 +6,7 @@ import sys
 import traceback
 
 parser = argparse.ArgumentParser(description='RingRobotX - By LingkongTeam')
-parser.add_argument('--no-cli', dest='cli', help='不需要CLI模式 | No CLI',action="store_true",default=False)
+parser.add_argument('--no-cli', dest='cli', help='不需要CLI模式 | No CLI', action="store_true", default=False)
 args = parser.parse_args()
 
 # 这里import model有作用，因为它会使init.py运行，进而初始化所有库文件
@@ -23,51 +23,58 @@ model.logger.moduleLoggerMain.info(str('''
     {} 加载完毕！
 ''').format(model.config.fastGetConfig("api-version")["RingRobotX"]))
 
-#============================ 重加载指令注册 ============================
+
+# ============================ 重加载指令注册 ============================
 
 def reload_com(string):
-    if string=="func":
+    if string == "func":
         model.func.reload()
-    elif string=="model":
+    elif string == "model":
         importlib.reload(model)
-    elif string=="all":
+    elif string == "all":
         python = sys.executable
         os.execl(python, python, *sys.argv)
     else:
         return "string is missing"
     return "success"
 
-model.cli.command_registry("reload",reload_com)
-model.cli.help_registry("reload","reload [model/func/all] | 重新加载模块")
+
+model.cli.command_registry("reload", reload_com)
+model.cli.help_registry("reload", "reload [model/func/all] | 重新加载模块")
+
 
 def exit_com():
     exit(0)
 
-model.cli.command_registry("exit",exit_com)
-model.cli.help_registry("exit","exit | 停止运行RingRobotX")
 
-#============================ 重加载指令注册 ============================
+model.cli.command_registry("exit", exit_com)
+model.cli.help_registry("exit", "exit | 停止运行RingRobotX")
+
+
+# ============================ 重加载指令注册 ============================
 
 def worker_cli():
-    cli=model.cli.console()
+    cli = model.cli.console()
     print("[RingRobotX] CLI模式启动。您现在可以输入")
     while True:
-        command=input("[RingRobotX] > ")
-        clist=command.split()
+        command = input("[RingRobotX] > ")
+        clist = command.split()
         try:
-            command=clist[0]
+            command = clist[0]
             clist.pop(0)
         except:
             model.logger.moduleLoggerMain.info("[CLI] 报告！您的命令无法解析。")
             model.logger.moduleLoggerMain.info(traceback.format_exc())
             continue
-        cli.commandRun(command,tuple(clist))
+        cli.commandRun(command, tuple(clist))
+
 
 def worker_2():
     model.logger.moduleLoggerMain.info("[RingRobotX] Worker schedule running!")
     while True:
         schedule.run_pending()
         time.sleep(1)
+
 
 worker = threading.Thread(target=worker_2, args=())
 worker.start()  # 开始循环
@@ -80,4 +87,5 @@ if __name__ == "__main__":
     time.sleep(2)
     model.hook.runhook_fast("RRCore.Main.Before.Running")
 else:
-    model.logger.moduleLoggerMain.info("[RingRobotX] 检测到模块形式导入，将不启动cli和beforeRunning Hook。请注意：这种启动方式会让其它脚本控制RingRobotX，请确认你启动的程序是你信任的程序。")
+    model.logger.moduleLoggerMain.info(
+        "[RingRobotX] 检测到模块形式导入，将不启动cli和beforeRunning Hook。请注意：这种启动方式会让其它脚本控制RingRobotX，请确认你启动的程序是你信任的程序。")

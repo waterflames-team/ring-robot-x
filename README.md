12fa34801bb2a59dcf777a823907b9da523b39ef
折腾调 <1410007625@qq.com>
2022-07-13 10:37:22 +0800

    :ambulance: 紧急修复：安装脚本snowboy复制出错

--- README.md
+++ README.md
@@ -97,9 +97,9 @@ git clone https://github.com/Kitt-AI/snowboy.git
 cd snowboy/swig/Python3
 make
 cd ../../../
-cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/Snowboy
-cp snowboy/examples/Python3/snowboydetect.py ring-robot-x/func_packages/Snowboy
-cp -a snowboy/resources/ ring-robot-x/func_packages/Snowboy/resources
+cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/ZZZ_Snowboy
+cp snowboy/examples/Python3/snowboydetect.py ring-robot-x/func_packages/ZZZ_Snowboy
+cp -a snowboy/resources/ ring-robot-x/func_packages/ZZZ_Snowboy/resources
 cd ../
 ```
 
--- install.sh
+++ install.sh
@@ -32,10 +32,10 @@ function install_snowboy(){
 function install_ringrobotx(){
   echo -e "\033[32;40m[INFO] Installing RingRobotX......\033[0m"
   git clone https://gitee.com/lkteam/ring-robot-x.git || error_dump "install_ringrobotx - git clone error"
-  cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/Snowboy || error_dump "install_ringrobotx - copy so error"
+  cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/ZZZ_Snowboy || error_dump "install_ringrobotx - copy so error"
   # cp snowboy/examples/Python3/snowboydecoder.py ring-robot-x/func_packages/Snowboy || error_dump "install_ringrobotx - copy decoder error"
-  cp snowboy/examples/Python3/snowboydetect.py ring-robot-x/func_packages/Snowboy || error_dump "install_ringrobotx - copy detect error"
-  cp -a snowboy/resources/ ring-robot-x/func_packages/Snowboy/resources || error_dump "install_ringrobotx - copy res error"
+  cp snowboy/examples/Python3/snowboydetect.py ring-robot-x/func_packages/ZZZ_Snowboy || error_dump "install_ringrobotx - copy detect error"
+  cp -a snowboy/resources/ ring-robot-x/func_packages/ZZZ_Snowboy/resources || error_dump "install_ringrobotx - copy res error"
   chmod 777 ../ringrobotx/ -R || error_dump "install_ringrobotx - chmod error"
   cd ring-robot-x || error_dump "install_ringrobotx - cd error"
   sudo pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple some-package || error_dump "install_before_require - Cannot install package"

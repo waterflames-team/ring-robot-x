996534750a7385a347abf6c4841ef4ce44221aea
折腾调 <1410007625@qq.com>
2022-09-25 12:12:19 +0800

    :sparkles: 支持firstload单个技能，优化文档

--- model/firstload.py
+++ model/firstload.py
@@ -10,9 +10,8 @@ import sys
 def import_func(path):
     return importlib.import_module(path)
 
-now=model.config.fastGetConfig("api-version")
-
 def firstload():
+    model.logger.moduleLoggerMain.info("[RingRobotX] 正在初始化（一般情况下，你可以通过 ctrl+c 的方式终止此进程，但是下次使用时仍会开始初始化）")
     path = os.getcwd() + "/func_packages/"
     name = os.path.basename(path)
 
@@ -20,12 +19,17 @@ def firstload():
 
     mypath = os.listdir(r'' + path)
     mypath.sort()
+    status=False
 
     for i in mypath:
         if os.path.isdir(path + "/" + i) and i != "__pycache__":
             try:
-                pack = import_func("func_packages." + i + ".setup")
-                pack.setup()
+                file_obj = open(path + "/" + i + "/config.json")
+                jso = json.loads(file_obj.read())
+                if jso["enable"] and jso["need_first_load"]:
+                    pack = import_func("func_packages." + i + ".setup")
+                    pack.setup()
+                    status=True
             except:
                 print("[本地] 初始化技能失败，忽略：" + i)
                 traceback.print_exc()
@@ -36,8 +40,10 @@ def firstload():
     pathn = model.config.APPConfig()
     pathn.setModelName("api-version")
     pathn.setConfig(json.dumps(now))
+    return status
 
-if now["first_load"]:
-    firstload()
+status=firstload()
+if status:
     model.logger.moduleLoggerMain.info("[RingRobotX] 初始化完成！正在重载程序......")
     os.execl(sys.executable, sys.executable, *sys.argv)
+model.logger.moduleLoggerMain.info("[RingRobotX] 初始化完成！没有需要初始化的技能，不需要重载。")

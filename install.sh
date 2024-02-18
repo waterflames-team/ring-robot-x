a4166dd81b073d1e06375bd0056021da4c5963d2
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2024-02-18 17:03:27 +0800

    🎨 Format `install.sh`.

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@ echo RingRobotX Installer
 echo Powered by Lingkong Team
 echo ----------------------------------
 
-function check_apt(){
+function check_apt() {
   local apt='0'
-  command -v $1 >/dev/null 2>&1 || { local apt='1'; }
+  command -v "$1" >/dev/null 2>&1 || { local apt='1'; }
 
   # fail on non-zero return value
   if [ "$apt" -ne 0 ]; then
-      echo -e "\033[31;40m[ERROR] apt command NOT found \033[0m"
-      exit 1
+    echo -e "\033[31;40m[ERROR] apt command NOT found \033[0m"
+    exit 1
   fi
 }
 
-function error_dump(){
+function error_dump() {
   echo -e "\033[31;40m[ERROR] $1 \033[0m"
   exit 1
 }
 
-function install_snowboy(){
+function install_snowboy() {
   echo -e "\033[32;40m[INFO] Installing snowboy requires......\033[0m"
   git clone https://gitee.com/zhetengtiao/snowboy.git || error_dump "install_snowboy - git clone error"
   cd snowboy/swig/Python3 || error_dump "install_snowboy - cd error"
@@ -29,7 +29,7 @@ function install_snowboy(){
   cd ../../../
 }
 
-function install_ringrobotx(){
+function install_ringrobotx() {
   echo -e "\033[32;40m[INFO] Installing RingRobotX......\033[0m"
   git clone https://gitee.com/waterflames-team/ring-robot-x.git -b develop || error_dump "install_ringrobotx - git clone error"
   cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/ZZZ_Snowboy || error_dump "install_ringrobotx - copy so error"
@@ -42,23 +42,22 @@ function install_ringrobotx(){
   python3 ring.py --firstload || error_dump "install_ringrobotx - firstload error"
 }
 
-function install_before_require(){
+function install_before_require() {
   require=(python3 python3-pip git python3-pyaudio swig libatlas-base-dev pulseaudio make alsa-utils sox libsox-fmt-mp3)
   echo -e "\033[32;40m[INFO] Installing requires......\033[0m"
-  for i in ${require[*]}
-  do
+  for i in ${require[*]}; do
     sudo apt install ${i} -y || error_dump "install_before_require - Cannot install package:${i}"
   done
   echo -e "\033[32;40m[INFO] apt install Success! \033[0m"
   echo -e "\033[32;40m[INFO] Success! \033[0m"
 }
 
-function before_install(){
+function before_install() {
   mkdir ringrobotx
   cd ringrobotx || error_dump "before_install - cd error"
 }
 
-function main(){
+function main() {
   check_apt "apt"
   before_install
   install_before_require

89a23d071fc311db510b7a9b0842c7e8692e3782
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2024-02-18 17:55:48 +0800

    ♻️ Refactor `install.sh`.

--- install.sh
+++ install.sh
@@ -4,66 +4,130 @@ echo RingRobotX Installer
 echo Powered by Lingkong Team
 echo ----------------------------------
 
-function check_apt() {
-  local apt='0'
-  command -v "$1" >/dev/null 2>&1 || { local apt='1'; }
+function log_info() {
+  echo -e "\033[32;40m[INFO] $1 \033[0m"
+}
 
-  # fail on non-zero return value
-  if [ "$apt" -ne 0 ]; then
-    echo -e "\033[31;40m[ERROR] apt command NOT found \033[0m"
-    exit 1
-  fi
+function log_error() {
+  echo -e "\033[31;40m[ERROR] $1 \033[0m"
+}
+
+function log_warn() {
+  echo -e "\033[33;40m[WARN] $1 \033[0m"
 }
 
 function error_dump() {
-  echo -e "\033[31;40m[ERROR] $1 \033[0m"
+  log_error "$1"
   exit 1
 }
 
+function check_program() {
+  log_info "Checking for $1 ..."
+
+  local installed='0'
+  path=$(command -v "$1") || { local installed='1'; }
+  # fail on non-zero return value
+  if [ "$installed" -ne 0 ]; then
+    error_dump "$1: command not found."
+  fi
+  log_info "Checking for $1 ... $path"
+}
+
+function install_system_pkgs() {
+  log_info "Installing system requirements ..."
+  sudo apt-get update || error_dump "install_system_pkgs - apt-get update error"
+  sudo apt-get install python3 python3-pip git python3-pyaudio swig libatlas-base-dev pulseaudio make alsa-utils sox libsox-fmt-mp3
+  log_info "Installing system requirements ... Done"
+}
+
 function install_snowboy() {
-  echo -e "\033[32;40m[INFO] Installing snowboy requires......\033[0m"
+  log_info "Installing requirement: 'snowboy' ..."
+  if [ -d "snowboy" ]; then
+    log_info "Snowboy already installed."
+    return
+  fi
   git clone https://gitee.com/zhetengtiao/snowboy.git || error_dump "install_snowboy - git clone error"
   cd snowboy/swig/Python3 || error_dump "install_snowboy - cd error"
   make || error_dump "install_snowboy - snowboy making error"
-  echo -e "\033[32;40m[INFO] Success! \033[0m"
   cd ../../../
+  log_info "Installing requirement: 'snowboy' ... Done"
 }
 
 function install_ringrobotx() {
-  echo -e "\033[32;40m[INFO] Installing RingRobotX......\033[0m"
+  log_info "Installing RingRobotX ..."
+  if [ -d "ring-robot-x" ]; then
+    log_info "RingRobotX already installed."
+    return
+  fi
   git clone https://gitee.com/waterflames-team/ring-robot-x.git -b develop || error_dump "install_ringrobotx - git clone error"
   cp snowboy/swig/Python3/_snowboydetect.so ring-robot-x/func_packages/ZZZ_Snowboy || error_dump "install_ringrobotx - copy so error"
   cp snowboy/examples/Python3/snowboydecoder.py ring-robot-x/func_packages/Snowboy || error_dump "install_ringrobotx - copy decoder error"
   cp snowboy/examples/Python3/snowboydetect.py ring-robot-x/func_packages/ZZZ_Snowboy || error_dump "install_ringrobotx - copy detect error"
   cp -a snowboy/resources/ ring-robot-x/func_packages/ZZZ_Snowboy/resources || error_dump "install_ringrobotx - copy res error"
-  chmod 777 ../ringrobotx/ -R || error_dump "install_ringrobotx - chmod error"
+  chmod 0777 ../ringrobotx/ -R || error_dump "install_ringrobotx - chmod error"
   cd ring-robot-x || error_dump "install_ringrobotx - cd error"
-  sudo pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple some-package || error_dump "install_before_require - Cannot install package"
+  if [ "$BREAK_SYSTEM_PACKAGES" -eq 1 ]; then
+    pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple some-package || error_dump "Install Python requirements - Cannot install package"
+  else
+    pip3 install -r requirements.txt --break-system-packages -i https://pypi.tuna.tsinghua.edu.cn/simple some-package || error_dump "Install Python requirements - Cannot install package"
+  fi
   python3 ring.py --firstload || error_dump "install_ringrobotx - firstload error"
+  log_info "Installing RingRobotX ... Done"
 }
 
-function install_before_require() {
-  require=(python3 python3-pip git python3-pyaudio swig libatlas-base-dev pulseaudio make alsa-utils sox libsox-fmt-mp3)
-  echo -e "\033[32;40m[INFO] Installing requires......\033[0m"
-  for i in ${require[*]}; do
-    sudo apt install ${i} -y || error_dump "install_before_require - Cannot install package:${i}"
-  done
-  echo -e "\033[32;40m[INFO] apt install Success! \033[0m"
-  echo -e "\033[32;40m[INFO] Success! \033[0m"
+function pre_install() {
+  mkdir -p ringrobotx
+  cd ringrobotx || error_dump "pre_install - cd error"
 }
 
-function before_install() {
-  mkdir ringrobotx
-  cd ringrobotx || error_dump "before_install - cd error"
+function is_venv() {
+  if [ -z "$VIRTUAL_ENV" ]; then
+    return 1
+  else
+    return 0
+  fi
+
+}
+
+function is_debian()
+{
+  if [ -f "/etc/os-release" ]; then
+    if grep -q "debian" "/etc/os-release"; then
+      return 0
+    fi
+  fi
+  return 1
 }
 
 function main() {
-  check_apt "apt"
-  before_install
-  install_before_require
+  declare BREAK_SYSTEM_PACKAGES=0
+  if is_debian; then
+    if is_venv; then
+      log_info "Running in venv."
+    else
+      log_warn "Cannot install pip packages on Debian, please use venv."
+      log_warn "We will install by passing '--break-system-packages'."
+      log_warn "Please make sure you know what you are doing."
+      echo "Press any key to continue, or press Ctrl+C to cancel."
+      read -n 1 -s -r
+      BREAK_SYSTEM_PACKAGES=1
+    fi
+  fi
+
+  install_system_pkgs
+
+  check_program "git"
+  check_program "make"
+  check_program "chmod"
+  check_program "pip3"
+  check_program "python3"
+  check_program "apt-get"
+  check_program "swig"
+
+  pre_install
   install_snowboy
   install_ringrobotx
-  echo -e "\033[32;40m[INFO] RingRobotX Ready! \033[0m"
+  log_info "RingRobotX Ready."
   exit
 }
 
